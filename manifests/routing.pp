365277f367b8ec192c2bb0c50a42392a72e5c0a4
Dries Dams <dries.dams@gmail.com>
2019-07-04 13:59:20 +0200

    add unless parameter to avoid continuous changes

--- manifests/routing.pp
+++ manifests/routing.pp
@@ -3,5 +3,6 @@ class jenkins::routing inherits jenkins{
     command =>  "iptables -A PREROUTING -t nat -i eth0 -p tcp --dport ${::jenkins::to_port} -j REDIRECT --to-port 8080",
     path    =>  '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     user    =>  'root',
+    unless  =>  'iptables -L | grep Jenkins',
   }
 }
